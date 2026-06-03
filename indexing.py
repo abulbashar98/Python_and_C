b946feb6be5c968cbc18474d49b40b74d21ccf06
Abul Bashar <abulbashar98116@gmail.com>
2026-06-03 14:38:46 +0200

    indexing in python

---
+++ indexing.py
@@ -0,0 +1,10 @@
+# indexing = accessing elements of a sequence using indexing operator []
+# [start:end:step]
+
+credit_number = "123446545672877"
+
+print(credit_number[-1::-1])
+#778276545644321
+
+print(credit_number[-1::-2])
+# 78755431
