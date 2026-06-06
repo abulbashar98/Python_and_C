a022de782ff9e36987f714c4ce2ddf63bbc9e29e
Abul Bashar <abulbashar98116@gmail.com>
2026-06-07 01:30:58 +0200

    instance 2

--- file_2_variadic_function.c
+++ file_2_variadic_function.c
@@ -1,7 +1,9 @@
+/*  1️⃣ Basic of variadic functions
+
 #include <stdio.h>
 #include <stdarg.h>
 
-// 1️⃣ Basic of variadic functions
+
 
 // Variadic function to add numbers
 int getsum(int n, ...){
@@ -38,5 +40,45 @@ int main(void){
     printf("Sum of these 3 elements(20,30,55): %d", getsum(3, 20,30,55));
 
 
+    return 0;
+}
+*/
+
+// 2️⃣ Another example of variadic function-> va_list,va_start,va_arg,va_end and ellipsis with first parameter of character pointer pointing types of arguments in a string.
+
+#include <stdio.h>
+#include <stdarg.h>
+
+// Variadic function receive a character pointer as 1st argument, that describes the types of argument we receive in ellipsis
+int print(const char* types, ...){
+    
+    
+    va_list list;
+
+    va_start(list, types);
+
+
+
+    for(int i = 0; types[i] != '\0';i++){
+        
+        
+        if(types[i] == 'i'){
+            int v = va_arg(list,int);
+            printf("Integer: %d\n", v);
+        }  
+        else{
+            double v = va_arg(list,double);
+            printf("Float: %.2f\n", v);
+        }
+    }
+    
+    va_end(list);
+    return 0;
+}
+
+
+int main(void){
+    
+    print("iddidi",8,8.4,4.4,5,3.14159,7);
     return 0;
 }
