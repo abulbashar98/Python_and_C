55ad381dd94e4e93d48dc50703608310d19f8fc8
Abul Bashar <abulbashar98116@gmail.com>
2026-06-12 20:59:42 +0200

    Introduction to inheritance in python

---
+++ Object_oriented_programming_in_python/introduction_to_inheritence.py
@@ -0,0 +1,51 @@
+# Inheritance = Allows to inherit attributes and methods from another class
+#               helps with code readability and extensibility
+#               class child(parent)
+
+class Animal:
+    def __init__(self,name):
+        self.name = name
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")        
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name} woofs!")
+        
+
+class Cat(Animal):
+    def speak(self):
+        print(f"{self.name} meows!")
+
+class Mouse(Animal):
+    def speak(self):
+        print(f"{self.name} squeeks!")
+
+# inherits name attribute from parent class animal
+dog = Dog("Scooby")
+cat = Cat("Tom")
+mouse = Mouse("Mickey")
+
+
+print(dog.name)
+print(cat.name)
+print(mouse.name)
+
+
+# inherits eat and sleep method from parent class animal
+dog.eat()
+dog.sleep()
+cat.eat()
+cat.sleep()
+mouse.eat()
+mouse.sleep()
+
+# having own methods and inherits an attribute name from parent class
+
+dog.speak()
+cat.speak()
+mouse.speak()
