56dceb0cbe2e11198badeef88ae708027375dcae
Abul Bashar <abulbashar98116@gmail.com>
2026-06-17 20:17:16 +0200

    PyQt5 Qlabels

--- PyQt5__gui_python/file_2_PyQt5_label copy.py
+++ PyQt5__gui_python/file_2_PyQt5_label copy.py
@@ -1,6 +1,8 @@
+# PyQt5 QLabels
+
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +10,13 @@ class MainWindow(QMainWindow):
         self.setGeometry(500,200, 500, 400)
         self.setWindowTitle("Your first PyQt5 cool GUI")
         self.setWindowIcon(QIcon("C:\\Users\\abulb\\OneDrive\\Pictures\\Screenshots\\67.jpg"))
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: grey;"
+                             "background-color: cyan;"
+                             "font-weight: bold;"
+                             "text-decoration: underline")
 
 
 def main():
